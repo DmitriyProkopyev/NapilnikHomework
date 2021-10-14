6d455e2121c68d76dcf1473b485da5fcdd3d0441
DmitriyProkopyev <prokopyev.d.a2005@gmail.com>
2021-10-14 20:14:00 +0500

    Added solution

---
+++ Solution.cs
@@ -0,0 +1,8 @@
+﻿public static int Clamp(int value, int minLimit, int maxLimit)
+{
+    if (value < minLimit)
+        return minLimit;
+    if (value > maxLimit)
+        return maxLimit;
+    return value;
+}
