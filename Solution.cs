3d0342207079adf28b3e2e4a3194b53b0b29f3e5
DmitriyProkopyev <prokopyev.d.a2005@gmail.com>
2021-10-14 20:27:27 +0500

    Added solution

---
+++ Solution.cs
@@ -0,0 +1,10 @@
+﻿class Weapon
+{
+    private int _bullets;
+
+    private const int BulletsPerShot = 1;
+
+    public bool CanShoot() => _bullets >= BulletsPerShot;
+
+    public void Shoot() => _bullets -= BulletsPerShot;
+}
