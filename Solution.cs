b24bb32bfd29df89ed90b21f6388bbac1afce899
DmitriyProkopyev <prokopye.d.a2005@gmail.com>
2021-10-15 10:39:09 +0500

    Added solution

---
+++ Solution.cs
@@ -0,0 +1,14 @@
+﻿public static void CreateNewObject()
+{
+
+}
+
+public static void RandomizeChance()
+{
+    _chance = Random.Range(0, 100);
+}
+
+public static int CalculateSalary(int hoursWorked)
+{
+    return _hourlyRate * hoursWorked;
+}
