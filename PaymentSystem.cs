c981cec723c7e53aaff0c177681a9b7ba1956bd9
DmitriyProkopyev <prokopye.d.a2005@gmail.com>
2021-10-15 12:24:17 +0500

    Added solution

---
+++ PaymentSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace IMJunior
+{
+    public abstract class PaymentSystem
+    {
+        public readonly string ID;
+
+        public PaymentSystem(string id)
+        {
+            ID = id;
+        }
+
+        public static PaymentSystem Choose(string id)
+        {
+            switch (id)
+            {
+                case "QIWI":
+                    return new Qiwi();
+                case "WebMoney":
+                    return new WebMoney();
+                case "Card":
+                    return new Card();
+            }
+
+            throw new ArgumentException(nameof(id));
+        }
+
+        public void ShowPaymentResult() => Console.WriteLine($"Проверка платежа через {ID}...");
+
+        public abstract void TryPay();
+    }
+}
