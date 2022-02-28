14c9ba0cd76e03b266a45b1ed6e716f9c3e5e9d8
DmitriyProkopyev <67915348+DmitriyProkopyev@users.noreply.github.com>
2022-02-28 17:00:02 +0500

    Made pathfinder which has only one logger

--- Pathfinder.cs
+++ Pathfinder.cs
@@ -4,16 +4,28 @@ namespace Lesson
 {
     internal class Pathfinder
     {
+<<<<<<< Updated upstream
         private readonly ILogger _logger;
 
         public Pathfinder(ILogger logger)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+=======
+        private ILogger _logger;
+
+        public Pathfinder(ILogger logger)
+        {
+            _logger = logger;
+>>>>>>> Stashed changes
         }
 
         public void Find()
         {
+<<<<<<< Updated upstream
             logger.WriteError("some message");
+=======
+            _logger.WriteError("some message");
+>>>>>>> Stashed changes
         }
     }
 }
